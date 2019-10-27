0d89ce00ea8168e127dfaa35101b4cb1f6f0db3d
ScottDenton <scottyd_14@hotmail.com>
2019-10-27 15:01:02 -0700

    updated exercise form and linked to muscle groups with basic views

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,37 @@
 class ExercisesController < ApplicationController
+
+  def index
+    @exercise = Exercise.new
+    @exercise_muscle = ExerciseMuscle.new
+    @exercises = Exercise.all
+  end
+
+  def create
+    @exercise = Exercise.create(name: exercise_params[:name])
+    exercise_params[:exercise_muscle][:id].each do |muscle_id|
+     ExerciseMuscle.create(
+      muscle_group_id: muscle_id.to_i,
+      exercise_id: @exercise.id
+    )
+    end
+    redirect_to exercises_path
+  end
+
+  def show
+    @exercise = Exercise.find(params[:id])
+  end
+
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(
+      :name,
+      {:exercise_muscle =>[
+        :id => []
+        ]
+      })
+  end
+
+
 end
