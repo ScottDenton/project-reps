8b13e6af12838303a1953919ed818248b60005a2
ScottDenton <scottyd_14@hotmail.com>
2019-11-30 19:14:42 -0800

    extra / removed

--- app/helpers/cardio_components_helper.rb
+++ app/helpers/cardio_components_helper.rb
@@ -1,5 +1,5 @@
 module CardioComponentsHelper
-  def cardio_time_helper(cardio_component)\
+  def cardio_time_helper(cardio_component)
     hours = cardio_component.hours ? cardio_component.hours: '00'
     minutes = cardio_component.minutes ? cardio_component.minutes: '00'
     seconds = cardio_component.seconds ? cardio_component.seconds: '00'
